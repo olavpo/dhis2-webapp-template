dfb8a8534d630ccc42cc968a2ac99144e2561825
Olav <op.registrering@me.com>
2024-11-23 09:30:56 +0100

    Update eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            require: true,
+            __dirname: true,
+            process: true,
+            module: true,
+            DHIS_CONFIG: true,
+        },
+
+        ecmaVersion: 9,
+        sourceType: "module",
+    },
+
+    rules: {
+        indent: ["error", 4],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "no-console": "off",
+    },
+}];
